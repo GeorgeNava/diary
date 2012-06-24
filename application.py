1ce9847027ae872d36413d5b5e80a9906f13a79c
George Nava <georgenava@gmail.com>
2012-06-24 12:13:10 -0530

    Removed app/json header

--- application.py
+++ application.py
@@ -18,19 +18,16 @@ def index():
 @app.route('/api/:date',method='GET')
 def apiGetActivities(date=None):
     data = models.getActivities(date)
-    app.response.set_header('content-type','application/json')
     return data
 
 @app.route('/api/:date',method='POST')
 def apiSaveActivity(date=None):
-    app.response.set_header('content-type','application/json')
     models.saveActivity(date,app.request.forms.get('text'))
     return '{"status":200}'
 
 @app.route('/api/:date',method='DELETE')
 def apiDropActivity(date=None):
     models.dropActivity(date)
-    app.response.set_header('content-type','application/json')
     return '{"status":200}'
 
 
